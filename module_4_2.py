28f50ff27afe5b2ca1c3dde0565140d526dc816b
igortadom <tadom@yandex.ru>
2024-08-24 10:46:10 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,41 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+test_function()
+# Вне функции test_function функция inner_function не видна.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
